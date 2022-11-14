43454dff083f59d9de640ea7630b2b6b9b5b9bdc
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-14 16:52:22 +0700

    point to the succeding node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
+    class Node
+    {
+        /* Node class represents the node of doubly linkedm list.
+         * it consists of the information part and links to
+         * its succeding and preceding
+         * in terms  of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+
+    }
     internal class Program
     {
         static void Main(string[] args)
